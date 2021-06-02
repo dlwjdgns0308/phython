3ae92f4fa2e820ada6e8e6886115da0c8cbb5d01
dlwjdgns0308 <dlwjdgns0308@naver.com>
2021-06-02 16:27:18 +0900

    s
    s

--- wepscraping_basic/4_re.py
+++ wepscraping_basic/4_re.py
@@ -27,5 +27,10 @@ def print_match(m):
 #m = p.search("good care") #search : 주어진 문자열 중에 일치하는게 있는지 확인
 #print_match(m)
 
-lst = p.findall("good care cafe") # findall : 일치하는 모든 것을 리스트 형태로 반환
-print(lst)
+# lst = p.findall("good care cafe") # findall : 일치하는 모든 것을 리스트 형태로 반환
+# print(lst)
+
+
+# 1. p = re.compile("원하는 형태")
+# 2. m = p.match("비교할 문자열") : 주어진 문자열의 처음부터 일치하는지 확인
+# 3. m 
--- wepscraping_basic/6_bs4.py
+++ wepscraping_basic/6_bs4.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://comic.naver.com/index.nhn"
+url = "https://comic.naver.com/webtoon/weekday.nhn"
 res = requests.get(url)
 res.raise_for_status()
 
@@ -12,4 +12,18 @@ soup = BeautifulSoup(res.text, "lxml")
 #print(soup.a.attrs) # a element 의 속성 정보를 출력
 #print(soup.a["href"])# a element 의 href 속성 '값' 정보를 출력
 
-soup.find("a",)
+print(soup.find("a", attrs={"class":"Nbtn_upload"})) # class="Nbtn_upload" 인 a element를 찾아줘
+rank1 = soup.find("li", attrs={"class":"rank01"})
+print(rank1.a.get_text())
+rank2 = rank1.next_sibling.next_sibling
+rank3 = rank2.next_sibling.next_sibling
+print(rank3.a.get_text())
+rank2 = rank3.previous_sibling.previous_sibling
+print(rank2.a.get_text())
+#print(rank1.parent)
+rank4 = rank1.find_next_sibling("li")
+print(rank4.a.get_text())
+print(rank1.find_next_siblings("li"))
+
+webtoon = soup.find("a", text="독립일기-11화 밤공기 딜레마")
+print(webtoon)
