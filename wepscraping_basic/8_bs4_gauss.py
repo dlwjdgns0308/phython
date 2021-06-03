10b6477a54cea2c8195524fbdd60082209b197d8
dlwjdgns0308 <dlwjdgns0308@naver.com>
2021-06-03 16:53:53 +0900

    s
    s

--- wepscraping_basic/8_bs4_gauss.py
+++ wepscraping_basic/8_bs4_gauss.py
@@ -6,14 +6,19 @@ res = requests.get(url)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml")
-cartoons = soup.find_all("td", attrs={"class":"title"})
+#cartoons = soup.find_all("td", attrs={"class":"title"})
+#만화 제목 + 링크 가져오기
 # title = cartoons[4].a.get_text()
 # link = cartoons[0].a["href"]
 # print(title)
 # print("https://comic.naver.com" + link)
 
+# 평점 구하기
+total_rates = 0
+cartoons = soup.find_all("div", attrs={"class":"rating_type"})
 for cartoon in cartoons:
-    link = cartoon.a["href"]
-    print(cartoon.a.get_text())
-    print("https://comic.naver.com" + link)
-    
+    rate = cartoon.find("strong").get_text()
+    print(rate)    
+    total_rates += float(rate)
+print("전체 점수 : ", total_rates)
+print("평균 점수 : ", total_rates / len(cartoons))
