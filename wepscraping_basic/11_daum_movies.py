069ef2f3ebb8593914ede475dfd02a199272bf75
dlwjdgns0308 <dlwjdgns0308@naver.com>
2021-06-09 15:02:45 +0900

    s
    s

---
+++ wepscraping_basic/11_daum_movies.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("https://search.daum.net/search?w=tot&q=2019%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR")
+res.raise_for_status()
+soup = BeautifulSoup(res.text, "lxml")
+
+images = soup.find_all("img", attrs={"class":"thumb_img"})
+
+for year in range(20):
+for idx, image in enumerate(images):
+    #print(image["src"])
+    image_url = image["src"]
+    if image_url.startswith("//"):
+        image_url = "https:" + image_url
+    
+
+    print(image_url)
+    image_res = requests.get(image_url)
+    image_res.raise_for_status()
+
+    with open("movie{}.jpg".format(idx+1), "wb") as f:
+        f.write(image_res.content) 
+
+    if idx >= 4: # 상위 5개 이미지만 받겠음
+        break
